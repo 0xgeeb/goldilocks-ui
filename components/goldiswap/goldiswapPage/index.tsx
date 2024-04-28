32b8617c600e2f646dc4fba94384c2da079c640a
0xgeeb <0xgeeb@gmail.com>
2024-04-27 22:57:05 -0400

    finished header and added inset to swap box

--- components/goldiswap/goldiswapPage/index.tsx
+++ components/goldiswap/goldiswapPage/index.tsx
@@ -2,23 +2,35 @@ export const GoldiswapPage = () => {
 
   return (
     <main className="w-screen h-screen">
-      <header className="w-[100%] h-[15%] bg-[#EEDCD2] flex flex-row items-center font-amaticbold">
-        <div className="">
-
+      <header className="w-[100%] h-[15%] bg-[#EEDCD2] flex flex-row items-center justify-between font-amaticbold text-[2vw] px-[4%]">
+        <div className="w-[18%] flex flex-row items-center hover:opacity-30 cursor-pointer">
+          <img className="w-[37%] h-[70%]" src="/logo.png" alt="logo" />
+          <h1>Goldilocks DAO</h1>
         </div>
-        <div className="w-[65.2%] h-[100%] flex flex-row items-center justify-between ml-[26.1%]">
-          <span>Wut is this?</span>
-          <span>Goldiswap</span>
-          <span>Stake</span>
-          <span>Borrow</span>
-          <span>Goldilend</span>
-          <span>Goldivaults</span>
+        <div className="w-[65.2%] h-[100%] flex flex-row items-center justify-between">
+          <span className="hover:scale-[130%] cursor-pointer">Wut is this?</span>
+          <span className="hover:scale-[130%] cursor-pointer">Goldiswap</span>
+          <span className="hover:scale-[130%] cursor-pointer">Stake</span>
+          <span className="hover:scale-[130%] cursor-pointer">Borrow</span>
+          <span className="hover:scale-[130%] cursor-pointer">Goldilend</span>
+          <span className="hover:scale-[130%] cursor-pointer">Goldivaults</span>
           <button>Connect</button>
         </div>
       </header>
       <div className="w-[100%] h-[85%] bg-cover bg-bottom bg-[url('/bg-goldiswap.png')] relative">
-        <div className="absolute top-[29.167%] left-[28.125%] w-[43.75%] h-[41.87%] border-2 border-black bg-[#D9C6BA] inset-4">
+        <h1 className="absolute top-[18%] left-[14%] text-[#D9C6BA] text-[8vw] font-amaticbold" id="page-title">SWAP</h1>
+        <div className="absolute top-[16.387%] left-[28.125%] w-[43.75%] h-[2.78%] bg-[#4D0B24] flex flex-row items-center justify-between px-2">
+          <span className="text-white font-baloo">$LOCKS floor price: $0.02</span>
+          <span className="text-white font-baloo">$LOCKS market price: $0.28</span>
+        </div>
+        <div className="absolute top-[19.167%] left-[28.125%] w-[43.75%] h-[41.87%] border-2 border-black bg-[#EEDCD2]">
+          <div className="absolute top-3 left-0 w-6 skew-y-[45deg] border-b-2 border-black"></div>
+          <div className="absolute bottom-3 left-0 w-6 -skew-y-[45deg] border-b-2 border-black"></div>
+          <div className="absolute top-3 right-0 w-6 -skew-y-[45deg] border-b-2 border-black"></div>
+          <div className="absolute bottom-3 right-0 w-6 skew-y-[45deg] border-b-2 border-black"></div>
+          <div className="absolute inset-6 border-2 border-black bg-[#D9C6BA]">
 
+          </div>
         </div>
       </div>
     </main>
