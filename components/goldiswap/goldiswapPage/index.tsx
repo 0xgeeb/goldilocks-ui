5b91d6b4b3bc8e415e68477bba23512ad87e6dd1
0xgeeb <0xgeeb@gmail.com>
2024-04-28 21:13:39 -0400

    added touchups to swap page

--- components/goldiswap/goldiswapPage/index.tsx
+++ components/goldiswap/goldiswapPage/index.tsx
@@ -14,11 +14,16 @@ export const GoldiswapPage = () => {
           <span className="hover:scale-[150%] cursor-pointer">Borrow</span>
           <span className="hover:scale-[150%] cursor-pointer">Goldilend</span>
           <span className="hover:scale-[150%] cursor-pointer">Goldivaults</span>
-          <button className="bg-[#E7B941] border-2 border-black px-9 py-2 hover:scale-[115%] cursor-pointer">Connect</button>
+          <button className="bg-[#E7B941] border-2 border-black px-9 py-2 hover:bg-[#F3AA8A] hover:scale-[110%] cursor-pointer">Connect</button>
         </div>
       </header>
       <div className="w-[100%] h-[85%] bg-cover bg-bottom bg-[url('/images/bg-goldiswap.png')] relative">
-        <h1 className="absolute top-[11%] left-[14%] text-[#D9C6BA] text-[8vw] font-amaticbold" id="page-title">SWAP</h1>
+        <div className="absolute h-[7.5%] w-[20.27%] top-[2.62%] left-[78.89%] flex flex-row items-center justify-between font-baloo font-semibold text-[1vw]">
+          <div className="w-[30.27%] h-[100%] flex items-center justify-center border-2 border-black bg-[#D9C6BA]">BUY</div>
+          <div className="w-[30.27%] h-[100%] flex items-center justify-center border-2 border-black bg-[#D9C6BA]">SELL</div>
+          <div className="w-[30.27%] h-[100%] flex items-center justify-center border-2 border-black bg-[#D9C6BA]">REDEEM</div>
+        </div>
+        <h1 className="absolute top-[12.16%] left-[14%] text-[#D9C6BA] text-[8vw] font-amaticbold" id="page-title">SWAP</h1>
         <div className="absolute top-[9.387%] left-[28.125%] w-[43.75%] h-[2.78%] bg-[#4D0B24] flex flex-row items-center justify-between px-2">
           <span className="text-white font-baloo">$LOCKS floor price: $0.02</span>
           <span className="text-white font-baloo">$LOCKS market price: $0.28</span>
@@ -28,8 +33,17 @@ export const GoldiswapPage = () => {
           <div className="absolute bottom-3 left-0 w-6 -skew-y-[45deg] border-b-2 border-black"></div>
           <div className="absolute top-3 right-0 w-6 -skew-y-[45deg] border-b-2 border-black"></div>
           <div className="absolute bottom-3 right-0 w-6 skew-y-[45deg] border-b-2 border-black"></div>
+          <span className="absolute bottom-[23%] left-[-7.5%] -rotate-[90deg] text-[0.8vw] font-baloo font-semibold">**0.3% fee on all buys**</span>
+          <span className="absolute top-[23%] right-[-5.8%] -rotate-[90deg] text-[0.8vw] font-baloo font-semibold">target ratio: 33.7%</span>
           <div className="absolute inset-6 border-2 border-black bg-[#D9C6BA]">
             <div className="w-[100%] h-[100%] relative flex flex-col">
+              <div className="flex flex-row absolute top-0 right-0 w-[33.61%] h-[10%] font-baloo font-semibold border-b-2 border-l-2 border-black">
+                <div className="flex flex-row items-center justify-center h-[100%] w-[25%] border-r-2 border-black bg-[#DCC2A8]">25%</div>
+                <div className="flex flex-row items-center justify-center h-[100%] w-[25%] border-r-2 border-black bg-[#D5A774]">50%</div>
+                <div className="flex flex-row items-center justify-center h-[100%] w-[25%] border-r-2 border-black bg-[#D19A5B]">75%</div>
+                <div className="flex flex-row items-center justify-center h-[100%] w-[25%] bg-[#CC8634]">MAX</div>
+              </div>
+              <img className="absolute h-6 w-6 top-[16%] left-[76%]" src="/images/icon-settings.png" alt="settings" />
               <div className="absolute top-[44%] left-[47.27%] bg-[#D9C6BA] z-10 h-10 w-10 border-2 border-black rounded-3xl flex justify-center items-center cursor-pointer hover:scale-110">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0D111C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>
               </div>
@@ -65,6 +79,13 @@ export const GoldiswapPage = () => {
           <span>$LOCKS supply: 100,000,000.64</span>
           <span>current fsl: 2,140,262.24</span>
           <span>current psl: 1,044,753.49</span>
+        </div>
+        <img className="absolute h-10 w-10 bottom-[3%] left-[3%]" src="/images/icon-share.png" alt="share" />
+        <div className="absolute bottom-[3%] right-[3%] flex flex-row items-center text-[#D9C6BA]">
+          <span className="font-amatic text-[1.3vw] mr-6">OOGA BOOGA</span>
+          <img className="w-8 h-8" src="/images/icon-x.png" alt="x" />
+          <img className="w-8 h-8" src="/images/icon-discord.png" alt="discord" />
+          <span className="text-[0.7vw] font-baloo ml-6">© 2024 Goldilocks DAO. All rights reserved.</span>
         </div>
       </div>
     </main>
