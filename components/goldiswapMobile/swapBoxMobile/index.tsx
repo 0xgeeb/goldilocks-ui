7ef6f2a2d4963d76f497eaf5ca9097c9a499cd91
0xgeeb <0xgeeb@gmail.com>
2024-05-08 18:23:36 -0400

    created outline for swapbox

--- components/goldiswapMobile/swapBoxMobile/index.tsx
+++ components/goldiswapMobile/swapBoxMobile/index.tsx
@@ -1,6 +1,14 @@
 export const SwapBoxMobile = () => {
 
   return (
-    <div></div>
+    <div className="absolute top-[4.6%] left-[15.5%] w-[69%] h-[55%] border-2 border-black bg-[#EEDCD2]">
+      <div className="absolute top-3 left-0 w-6 skew-y-[45deg] border-b-2 border-black"></div>
+      <div className="absolute bottom-3 left-0 w-6 -skew-y-[45deg] border-b-2 border-black"></div>
+      <div className="absolute top-3 right-0 w-6 -skew-y-[45deg] border-b-2 border-black"></div>
+      <div className="absolute bottom-3 right-0 w-6 skew-y-[45deg] border-b-2 border-black"></div>
+      <div className="absolute inset-3 border-2 border-black bg-[#D9C6BA]">
+
+      </div>
+    </div>
   )
 }
