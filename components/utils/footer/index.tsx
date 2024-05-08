e0c4854a09855ab829671e3a2087e06c5204b9fb
0xgeeb <0xgeeb@gmail.com>
2024-05-08 12:58:47 -0400

    finished nav bar buttons

--- components/utils/footer/index.tsx
+++ components/utils/footer/index.tsx
@@ -4,10 +4,10 @@ export const Footer = () => {
       <img className="absolute h-10 w-10 bottom-[3%] left-[3%]" src="/images/icon-share.png" alt="share" />
       <div className="absolute bottom-[3%] right-[3%] flex flex-row items-center text-[#D9C6BA]">
         <span className="font-amatic text-[1.3vw] mr-6">OOGA BOOGA</span>
-        <a className="cursor-pointer hover:scale-110" href="https:x.com/goldilocksmoney">
+        <a className="cursor-pointer hover:scale-110" href="https:x.com/goldilocksmoney" target="_blank">
           <img className="w-8 h-8" src="/images/icon-x.png" alt="x" />
         </a>
-        <a className="cursor-pointer hover:scale-110" href="https://discord.gg/3cdn88Mbq8">
+        <a className="cursor-pointer hover:scale-110" href="https://discord.gg/3cdn88Mbq8" target="_blank">
           <img className="w-8 h-8" src="/images/icon-discord.png" alt="discord" />
         </a>
         <span className="text-[0.7vw] font-baloo ml-6">© 2024 Goldilocks DAO. All rights reserved.</span>
--- components/utils/navBarButtons/index.tsx
+++ components/utils/navBarButtons/index.tsx
@@ -4,8 +4,8 @@ export const NavBarButtons = () => {
 
   return (
     <div className="h-[89%] w-[100%] bg-[#E4B19B]">
-      <div className="h-[100%] w-[100%] py-[8%] flex flex-col justify-between">
-        <div className="w-[100%] h-[80%] flex flex-col items-center justify-between font-amaticbold text-[8vw]">
+      <div className="h-[100%] w-[100%] py-[8%] flex flex-col items-center justify-between">
+        <div className="w-[100%] h-[85%] flex flex-col items-center justify-between font-amaticbold text-[8vw]">
           <ConnectButton.Custom>
             {({
               account,
@@ -43,8 +43,12 @@ export const NavBarButtons = () => {
           <a href="/goldilend" className="hover:scale-[150%] cursor-pointer"><span>Goldilend</span></a>
           <a href="/goldivaults" className="hover:scale-[150%] cursor-pointer"><span>Goldivaults</span></a>
         </div>
-        <div className="w-[100%] h-[25%] flex flex-row items-center justify-center font-amatic text-[4vw]">
-          <h1>ooga booga</h1>
+        <div className="w-[50%] h-[25%] flex flex-row items-center justify-between">
+          <h1 className="font-amaticbold text-[7vw]">ooga booga</h1>
+          <div className="h-[100%] w-[45%] mt-2 flex flex-row items-center">
+            <a className="hover:scale-[150%] cursor-pointer w-[50%]" href="https:x.com/goldilocksmoney" target="_blank"><img className="" src="/images/icon-x-dark.png" alt="twitter" /></a>
+            <a href="hover:scale-[150%] cursor-pointer w-[50%] https://discord.gg/3cdn88Mbq8" target="_blank"><img className="" src="/images/icon-discord-dark.png" alt="discord" /></a>
+          </div>
         </div>
       </div>
     </div>
