adebf91b031f52953987f634dc63d23d203a397e
0xgeeb <0xgeeb@gmail.com>
2024-05-14 21:40:33 -0400

    meant stats in previous 2 commits lol. now fixing footer

--- components/utils/footerMobile/index.tsx
+++ components/utils/footerMobile/index.tsx
@@ -2,8 +2,8 @@ export const FooterMobile = () => {
 
   return (
     //todo: doesnt look good on smaller phones
-    <div className="absolute top-[88%] left-[20%] h-[9%] w-[65%] flex flex-col items-center justify-around">
-      <div className="w-[70%] flex flex-row items-center justify-between">
+    <div className="absolute top-[88%] left-[5%] h-[9%] w-[90%] flex flex-col items-center justify-around">
+      <div className="w-[50%] flex flex-row items-center justify-between">
         <span className="text-[6vw] font-amaticbold text-[#D9C6BA]">ooga booga</span>
         <div className="flex flex-row items-center justify-end w-[50%]">
           <a className="focus:scale-[150%] cursor-pointer w-[40%]" href="https://x.com/goldilocksmoney" target="_blank"><img className="" src="/images/icon-x.png" alt="twitter" /></a>
