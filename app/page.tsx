41644ecfb8dfab0afdbd81584a499a99c453b796
0xgeeb <0xgeeb@gmail.com>
2024-06-07 20:31:07 -0400

    created homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-"use client"
+import type { Metadata } from "next"
+import { HomePage } from "../components/home"
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: "mf goldilocks",
+  description: "Berachain Defi"
+}
 
+export default function Goldilend() {
+  
   return (
-    <div>blame napzilla not me</div>
+    <HomePage />
   )
 }
