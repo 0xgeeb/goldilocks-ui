a35deefff61b850b9f5aa3a518738ad8b92094f7
0xgeeb <0xgeeb@gmail.com>
2024-06-10 09:33:33 -0400

    fixed 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,42 +1,26 @@
 export default function NotFound() {
   
-  //todo: needs responsiveness, mobile, and new navbar
   return (
     <div className="w-screen h-screen">
-      <header className="w-[100%] h-[15%] bg-[#EEDCD2] flex flex-row items-center justify-between font-amaticbold text-[2vw] px-[4%]">
-        <div className="w-[18%] flex flex-row items-center hover:opacity-30 cursor-pointer">
-          <img className="w-[37%] h-[70%]" src="/images/logo-goldilocks.png" alt="logo" />
-          <h1 className="text-[2.4vw]">Goldilocks DAO</h1>
-        </div>
-        <div className="w-[65.2%] h-[100%] flex flex-row items-center justify-between">
-          <a 
-            href="/wut"
-            className="hover:scale-[150%] cursor-pointer"
-          >
-            <span>Wut is this?</span>
-          </a>
-          <a 
-            href="/goldiswap"
-            className="hover:scale-[150%] cursor-pointer"
-          >
-            <span>Goldiswap</span>
-          </a>
-          <a 
-            href="/goldilend/borrow"
-            className="hover:scale-[150%] cursor-pointer"
-          >
-            <span>Goldilend</span>
-          </a>
-          <a 
-            href="/goldivaults"
-            className="hover:scale-[150%] cursor-pointer"
-          >
-            <span>Goldivaults</span>
-          </a>
+      <header className="w-[100%] h-[11%] lg:h-[15%] bg-[#EEDCD2] flex flex-row items-center justify-between font-amaticbold px-[4%]">
+        <a
+          href="/"
+          className="w-[25%] lg:w-[18%]"
+        >
+          <div className="w-[100%] flex flex-row items-center hover:opacity-30 cursor-pointer">
+            <img className="w-[30%] lg:w-[37%] h-[70%]" src="/images/logo-goldilocks.png" alt="logo" />
+            <h1 className="text-[3vw] lg:text-[2.4vw]">Goldilocks DAO</h1>
+          </div>
+        </a>
+        <div className="w-[65%] sm:w-[55%] lg:w-[45%] h-[100%] flex flex-row items-center justify-between text-[3.5vw] sm:text-[3vw] lg:text-[2.2vw]">
+          <a href="/wut" className="hover:scale-[150%] cursor-pointer"><span>Wut is this?</span></a>
+          <a href="/goldiswap/swap" className="hover:scale-[150%] cursor-pointer"><span>Goldiswap</span></a>
+          <a href="/goldilend/borrow" className="hover:scale-[150%] cursor-pointer"><span>Goldilend</span></a>
+          <a href="/goldivaults" className="hover:scale-[150%] cursor-pointer"><span>Goldivaults</span></a>
         </div>
       </header>
-      <div className="w-[100%] h-[85%] flex flex-col items-center justify-center bg-[#EEDCD2]">
-        <h1 className="font-amaticbold text-[7vw] text-[#E7B941]">where r u</h1>
+      <div className="w-[100%] h-[89%] lg:h-[85%] flex flex-col items-center justify-center bg-[#EEDCD2]">
+        <h1 className="font-amaticbold text-[9vw] lg:text-[7vw] text-[#E7B941]">where r u</h1>
         <img className="h-[60%]" src="/images/icon-not-found.png" alt="icon" />
       </div>
     </div>
--- components/home/homePage/index.tsx
+++ components/home/homePage/index.tsx
@@ -5,7 +5,7 @@ export const HomePage = () => {
   return (
     // isDesktop ?
     <main className="w-screen h-screen">
-      <header className="w-[100%] h-[11%] lg:h-[15%] bg-[#EEDCD2] flex flex-row items-center justify-between font-amaticbold px-[2%] lg:px-[4%]">
+      <header className="w-[100%] h-[11%] lg:h-[15%] bg-[#EEDCD2] flex flex-row items-center justify-between font-amaticbold px-[4%]">
         <a
           href="/"
           className="w-[25%] lg:w-[18%]"
@@ -15,7 +15,7 @@ export const HomePage = () => {
             <h1 className="text-[3vw] lg:text-[2.4vw]">Goldilocks DAO</h1>
           </div>
         </a>
-        <div className="w-[80%] lg:w-[45%] h-[100%] flex flex-row items-center justify-between text-[2.8vw] lg:text-[2.2vw]">
+        <div className="w-[55%] lg:w-[45%] h-[100%] flex flex-row items-center justify-between text-[3vw] lg:text-[2.2vw]">
           <a href="/wut" className="hover:scale-[150%] cursor-pointer"><span>Wut is this?</span></a>
           <a href="/goldiswap/swap" className="hover:scale-[150%] cursor-pointer"><span>Goldiswap</span></a>
           <a href="/goldilend/borrow" className="hover:scale-[150%] cursor-pointer"><span>Goldilend</span></a>
