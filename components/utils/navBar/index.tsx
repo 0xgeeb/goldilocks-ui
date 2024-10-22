ea08c2af437787e22014c21b16655c268c218719
0xgeeb <0xgeeb@gmail.com>
2024-10-22 22:45:00 +0900

    fixed navbar with vault address

--- components/utils/navBar/index.tsx
+++ components/utils/navBar/index.tsx
@@ -78,20 +78,20 @@ export const NavBar = ({ wutPopup, setWutPopup }: NavBarProps) => {
               <a className="text-[#DB7200]"><span>Goldivaults</span></a>
               <a href="/goldivault/vaults" className="hover:scale-[150%] cursor-pointer text-[#DB7200] underline underline-offset-8 text-[2.1vw] xl:text-[1.6vw]"><span>Vaults</span></a>
               <a href="/goldivault/pools" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>Pools</span></a>
-              <a href="/goldivault/vault/0xCfeC57e0a0c8a428E335a4222B1558e30A1F4517" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>bHoney</span></a>
+              <a href="/goldivault/vault/0x541C4aCA915ccC83B1bf48b510D1653cba61115F" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>bHoney</span></a>
             </> :
             pathname === '/goldivault/pools' ?
             <>
               <a className="text-[#DB7200]"><span>Goldivaults</span></a>
               <a href="/goldivault/vaults" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>Vaults</span></a>
               <a href="/goldivault/pools" className="hover:scale-[150%] cursor-pointer text-[#DB7200] underline underline-offset-8 text-[2.1vw] xl:text-[1.6vw]"><span>Pools</span></a>
-              <a href="/goldivault/vault/0xCfeC57e0a0c8a428E335a4222B1558e30A1F4517" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>bHoney</span></a>
+              <a href="/goldivault/vault/0x541C4aCA915ccC83B1bf48b510D1653cba61115F" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>bHoney</span></a>
             </> :
-            pathname === '/goldivault/vault/0xCfeC57e0a0c8a428E335a4222B1558e30A1F4517' ?
+            pathname === '/goldivault/vault/0x541C4aCA915ccC83B1bf48b510D1653cba61115F' ?
             <>
               <a href="/goldivault/vaults" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>Vaults</span></a>
               <a href="/goldivault/pools" className="hover:scale-[150%] cursor-pointer text-[2.1vw] xl:text-[1.6vw]"><span>Pools</span></a>
-              <a href="/goldivault/vault/0xCfeC57e0a0c8a428E335a4222B1558e30A1F4517" className="hover:scale-[150%] cursor-pointer text-[#DB7200] underline underline-offset-8 text-[2.1vw] xl:text-[1.6vw]"><span>bHoney</span></a>
+              <a href="/goldivault/vault/0x541C4aCA915ccC83B1bf48b510D1653cba61115F" className="hover:scale-[150%] cursor-pointer text-[#DB7200] underline underline-offset-8 text-[2.1vw] xl:text-[1.6vw]"><span>bHoney</span></a>
             </> :
             <a href="/goldivault/vaults" className="hover:scale-[150%] cursor-pointer"><span>Goldivaults</span></a>
           }
