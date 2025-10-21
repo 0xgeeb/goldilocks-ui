c3e46567d3e2c865fce0534d427f5da2a033d5a9
0xgeeb <0xgeeb@gmail.com>
2025-10-21 10:25:46 -0400

    fix proposal page

--- components/govMobile/proposalPageMobile/index.tsx
+++ components/govMobile/proposalPageMobile/index.tsx
@@ -13,22 +13,24 @@ export const ProposalPageMobile = ({ number }: BoxProps) => {
   const { navButtonsOpen } = useDesktop();
 
   return (
-    <main className="h-screen w-screen">
+    <main className="flex min-h-screen w-screen flex-col">
       <NavBarMobile />
       {navButtonsOpen && <NavBarButtons />}
       {!navButtonsOpen && (
-        <div className="relative h-[89%] w-full bg-[url('/images/bg-goldiswap-mobile.png')] bg-cover">
-          <h1
-            className="absolute left-[3%] top-[0.5%] font-amaticbold text-[12vw] text-[#D9C6BA]"
-            id="page-title"
-          >
-            Proposal #{number}
-          </h1>
-          <ProposalBoxMobile number={number} />
-          <ProposalButtonsMobile number={number} />
-          <ProposalsFetcher />
+        <>
+          <div className="relative flex-1 w-full bg-[url('/images/bg-goldiswap-mobile.png')] bg-cover">
+            <h1
+              className="absolute left-[3%] top-[0.5%] font-amaticbold text-[12vw] text-[#D9C6BA]"
+              id="page-title"
+            >
+              Proposal #{number}
+            </h1>
+            <ProposalBoxMobile number={number} />
+            <ProposalButtonsMobile number={number} />
+            <ProposalsFetcher />
+          </div>
           <FooterMobile />
-        </div>
+        </>
       )}
     </main>
   );
